d5c3ee0551b482b1b85b4c8c35f194aa41c689d1
Simon Giegerich <simon.giegerich@stud.tu-darmstadt.de>
2023-06-29 16:24:13 +0200

    initial setup for asterix

---
+++ asterix/asterix_l3_plugins/include/asterix_l3_plugins/asterix_kinematics.h
@@ -0,0 +1,75 @@
+// =================================================================================================
+// Copyright (c) 2023, Simon Giegerich, Technische Universität Darmstadt
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this
+//    list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its
+//    contributors may be used to endorse or promote products derived from
+//    this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// =================================================================================================
+
+#ifndef ASTERIX_KINEMATICS_PLUGIN_H__
+#define ASTERIX_KINEMATICS_PLUGIN_H__
+
+#include <ros/ros.h>
+
+#include <l3_plugins/std/kdl_kinematics.h>
+
+namespace asterix_l3
+{
+using namespace l3;
+
+enum LEG_IDS
+{
+  L_TRACK = 0,
+  R_TRACK = 1
+};
+
+class AsterixKinematicsPlugin : public KdlKinematics
+{
+public:
+  // typedefs
+  typedef boost::shared_ptr<AsterixKinematicsPlugin> Ptr;
+  typedef boost::shared_ptr<const AsterixKinematicsPlugin> ConstPtr;
+
+  AsterixKinematicsPlugin();
+
+  bool loadParams( const vigir_generic_params::ParameterSet &params ) override;
+
+  bool initialize( const vigir_generic_params::ParameterSet &params ) override;
+
+  Pose calcFeetCenter( const FootholdArray &footholds ) const override;
+
+  Pose calcFeetCenter( const FootholdConstPtrArray &footholds ) const override;
+
+  bool calcLegIK( const Pose &base_pose, const Foothold &foothold, const std::vector<double> &cur_q,
+                  std::vector<double> &q ) const override;
+
+protected:
+  std::vector<std::vector<double>> neutral_stance_;
+
+  bool use_ball_foot_;
+};
+}  // namespace asterix_l3
+
+#endif
